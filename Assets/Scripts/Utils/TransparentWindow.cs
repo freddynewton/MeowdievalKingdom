330f5a2abfecbab5a178aa1acea06224f286b06e
Freddy Newton <freddakdogan2@gmail.com>
2025-03-20 16:51:59 +0100

    Add ProPixelizer, Change Default Scene and update Camera Settings, Finish TransparentWindow - Now it will always be in focus and interactable

--- Assets/Scripts/Utils/TransparentWindow.cs
+++ Assets/Scripts/Utils/TransparentWindow.cs
@@ -4,73 +4,113 @@ using UnityEngine;
 
 namespace Meowdieval.Core.Utils
 {
+    /// <summary>
+    /// This class makes the Unity window transparent and ensures it is always on top.
+    /// It also handles click-through functionality based on whether the pointer is over a UI element.
+    /// </summary>
     public class TransparentWindow : MonoBehaviour
     {
-#if !UNITY_EDITOR
-            // Import Windows API functions
-            [DllImport("user32.dll")] private static extern IntPtr GetActiveWindow();
-            [DllImport("user32.dll", SetLastError = true)] private static extern int SetWindowLong(IntPtr hWnd, int nIndex, uint dwNewLong);
-            [DllImport("user32.dll", SetLastError = true)] private static extern uint GetWindowLong(IntPtr hWnd, int nIndex);
-            [DllImport("user32.dll")][return: MarshalAs(UnmanagedType.Bool)] private static extern bool SetLayeredWindowAttributes(IntPtr hwnd, uint crKey, byte bAlpha, uint dwFlags);
-            [DllImport("user32.dll", SetLastError = true)] private static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
+        // Import Windows API functions
+        [DllImport("user32.dll")] private static extern IntPtr GetActiveWindow();
+        [DllImport("user32.dll", SetLastError = true)] private static extern int SetWindowLong(IntPtr hWnd, int nIndex, uint dwNewLong);
+        [DllImport("user32.dll", SetLastError = true)] private static extern uint GetWindowLong(IntPtr hWnd, int nIndex);
+        [DllImport("user32.dll")][return: MarshalAs(UnmanagedType.Bool)] private static extern bool SetLayeredWindowAttributes(IntPtr hwnd, uint crKey, byte bAlpha, uint dwFlags);
+        [DllImport("user32.dll", SetLastError = true)] private static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
+        [DllImport("user32.dll")] private static extern bool SetForegroundWindow(IntPtr hWnd);
 
-            private const int GWL_EXSTYLE = -20;
-            private const uint WS_EX_LAYERED = 0x00080000;
-            private const uint WS_EX_TRANSPARENT = 0x00000020;
-            private const uint LWA_COLORKEY = 0x00000001;
-            private const uint LWA_ALPHA = 0x00000002;
-            private static readonly IntPtr HWND_TOPMOST = new IntPtr(-1);
+        private const int GWL_EXSTYLE = -20;
+        private const uint WS_EX_LAYERED = 0x00080000;
+        private const uint WS_EX_TRANSPARENT = 0x00000020;
+        private const uint LWA_COLORKEY = 0x00000001;
+        private static readonly IntPtr HWND_TOPMOST = new IntPtr(-1);
 
-            public event Action<bool> OnClickthroughChanged;
+        public event Action<bool> OnClickthroughChanged;
 
-            private void Start()
+        private bool isClickthrough = false;
+        private IntPtr hWnd;
+
+        /// <summary>
+        /// Initializes the window settings to make it transparent and always on top.
+        /// </summary>
+        private void Start()
+        {
+            // Get the current window handle
+            hWnd = GetActiveWindow();
+
+            // Set the window to always be on top
+            SetWindowPos(hWnd, HWND_TOPMOST, 0, 0, 0, 0, 0);
+
+            // Set the window to be transparent
+            uint windowLong = GetWindowLong(hWnd, GWL_EXSTYLE);
+            SetWindowLong(hWnd, GWL_EXSTYLE, windowLong | WS_EX_LAYERED);
+
+            // Set the background to be transparent
+            SetLayeredWindowAttributes(hWnd, 0, 0, LWA_COLORKEY);
+
+            // Ensure the window is in focus
+            SetForegroundWindow(hWnd);
+        }
+
+        /// <summary>
+        /// Updates the click-through state based on whether the pointer is over a UI element.
+        /// Ensures the window remains in focus when clicked.
+        /// </summary>
+        private void Update()
+        {
+            bool isOverElement = IsPointerOverElement();
+            SetClickthrough(!isOverElement);
+
+            // Ensure the window regains focus when an element is clicked
+            if (Input.GetMouseButtonDown(0) && isOverElement)
             {
-                // Get the current window handle
-                IntPtr hWnd = GetActiveWindow();
+                SetForegroundWindow(hWnd);
+            }
+        }
 
-                // Set the window to always be on top
-                SetWindowPos(hWnd, HWND_TOPMOST, 0, 0, 0, 0, 0);
-
-                // Set the window to be transparent
-                uint windowLong = GetWindowLong(hWnd, GWL_EXSTYLE);
-                SetWindowLong(hWnd, GWL_EXSTYLE, windowLong | WS_EX_LAYERED | WS_EX_TRANSPARENT);
-                // Set the background to be transparent
-                SetLayeredWindowAttributes(hWnd, 0, 0, LWA_COLORKEY);
+        /// <summary>
+        /// Checks if the pointer is over a UI element.
+        /// </summary>
+        /// <returns>True if the pointer is over a UI element, otherwise false.</returns>
+        private bool IsPointerOverElement()
+        {
+            // Check for 2D UI elements
+            if (Physics2D.OverlapPoint(Camera.main.ScreenToWorldPoint(Input.mousePosition)) != null)
+            {
+                return true;
             }
 
-            private void Update()
+            // Check for 3D UI elements
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out RaycastHit hit) && hit.collider != null)
             {
-                bool isOverUI = IsPointerOverUIElement();
-                SetClickthrough(!isOverUI);
+                return true;
             }
 
-            private bool IsPointerOverUIElement()
+            return false;
+        }
+
+        /// <summary>
+        /// Sets the click-through state of the window.
+        /// </summary>
+        /// <param name="clickthrough">True to enable click-through, false to disable.</param>
+        private void SetClickthrough(bool clickthrough)
+        {
+            if (clickthrough == isClickthrough) return;
+
+            isClickthrough = clickthrough;
+            uint exStyle = GetWindowLong(hWnd, GWL_EXSTYLE);
+
+            if (clickthrough)
             {
-                // Check for 2D UI elements
-                if (Physics2D.OverlapPoint(Camera.main.ScreenToWorldPoint(Input.mousePosition)) != null)
-                    return true;
-
-                // Check for 3D UI elements
-                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-                if (Physics.Raycast(ray, out RaycastHit hit))
-                    return hit.collider != null;
-
-                return false;
+                exStyle |= WS_EX_TRANSPARENT;
+            }
+            else
+            {
+                exStyle &= ~WS_EX_TRANSPARENT;
             }
 
-            private void SetClickthrough(bool clickthrough)
-            {
-                if (clickthrough)
-                {
-                    SetWindowLong(GetActiveWindow(), GWL_EXSTYLE, WS_EX_LAYERED | WS_EX_TRANSPARENT);
-                }
-                else
-                {
-                    SetWindowLong(GetActiveWindow(), GWL_EXSTYLE, WS_EX_LAYERED);
-                }
-
-                OnClickthroughChanged?.Invoke(clickthrough);
-            }
-#endif
+            SetWindowLong(hWnd, GWL_EXSTYLE, exStyle);
+            OnClickthroughChanged?.Invoke(clickthrough);
+        }
     }
 }
